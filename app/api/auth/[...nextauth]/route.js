c6a9ceae8c8d39881fee112968cc487c51ea4725
Disura Chathwara <disura2005@gmail.com>
2023-09-02 19:40:29 +0530

    h

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-import User from '@models/User';
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
